e795fe3f029ab231230d24594a3856618053f1a5
cody-white-tiger <hddfilestorage@gmail.com>
2013-09-04 20:52:11 -0500

    Add application

--- Timeline/src/Start/Start.java
+++ Timeline/src/Start/Start.java
@@ -1,5 +1,31 @@
 package Start;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class Start {
 
+	public static JFrame MakeGUI()
+	{
+		System.out.println("Making GUI");
+		JFrame mainFrame = new JFrame("William's Timeline");
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel emptyLabel = new JLabel("");
+		emptyLabel.setPreferredSize(new Dimension(568, 100));
+		mainFrame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+		mainFrame.pack();
+		return mainFrame;
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Starting");
+		JFrame Screen = MakeGUI();
+		Screen.setVisible(true);
+	}
+	
 }
